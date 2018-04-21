a59cba14d79778a7487259424de246bbf6b703bb
Rippr <ishanmanchanda70@gmail.com>
2018-04-21 17:34:03 +0530

    Added some more questions

---
+++ CodeChef/COMPETE/Code Melange - CMEL2018/Candy Love - CNDLOVE.py
@@ -0,0 +1,13 @@
+def main():
+	from sys import stdin, stdout
+	rl = stdin.readline
+	wl = stdout.write
+	int1 = int
+
+	t = int1(rl())
+	for i in range(t):
+		rl()
+		wl("NO\n" if sum([int1(x) for x in rl().split()]) % 2 == 0 else "YES\n")
+
+
+main()
--- CodeChef/COMPETE/Code Melange - CMEL2018/The Deadly Sin - SINS.py
+++ CodeChef/COMPETE/Code Melange - CMEL2018/The Deadly Sin - SINS.py
@@ -3,15 +3,19 @@ def main():
 	rl = stdin.readline
 	wl = stdout.write
 	int1 = int
+	str1 = str
 
 	t = int1(rl())
 	for i in range(t):
 		x, y = [int1(x) for x in rl().split()]
-		n = y - x
-		a = 4 * x * x
-		b = ((2 * n) + 1) * ((2 * n) - 1) / 3
-		c = 4 * n
-		wl(str(int((n * ((a + b + c) % 1000000007)) % 1000000007)))
+		while x != y and x != 0 and y != 0:
+			if x > y:
+				a = x % y
+				x = a if a != 0 else y
+			else:
+				a = y % x
+				y = a if a != 0 else x
+		wl(str1(x + y) + "\n")
 
 
 main()
