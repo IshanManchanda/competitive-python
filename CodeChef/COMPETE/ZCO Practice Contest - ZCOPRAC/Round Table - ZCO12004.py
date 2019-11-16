972759baa98a2a2a4056718e234107ffafb72a63
Ishan Manchanda <ishanmanchanda70@gmail.com>
2019-11-16 21:32:46 +0530

    Updated solution for Round Table

--- CodeChef/COMPETE/ZCO Practice Contest - ZCOPRAC/Round Table - ZCO12004.py
+++ CodeChef/COMPETE/ZCO Practice Contest - ZCOPRAC/Round Table - ZCO12004.py
@@ -1,36 +1,35 @@
-dp = [0] + [-1] * 10000000
+# https://www.codechef.com/ZCOPRAC/problems/ZCO12004
+dp = [[-1] * int(1.1e6), [-1] * int(1.1e6)]
 
 
-def solve(a, n):
-	global dp
-	if dp[n] != -1:
-		return dp[n]
-	if n == 0:
-		return 0
-	if n == 1:
-		dp[1] = a[0]
-		return dp[1]
-	if n == 2:
-		dp[2] = a[1]
-		return dp[2]
-	dp[n] = a[n - 1] + min(solve(a, n - 1), solve(a, n - 2))
-	return dp[n]
+def solve(a, d, l):
+	if dp[l][d] != -1:
+		return dp[l][d]
+
+	if d == 0:
+		return a[0]
+	if d == 1:
+		return a[1] if l else a[1] + a[0]
+
+	dp[l][d] = a[d] + min(
+		solve(a, d - 1, l),
+		solve(a, d - 2, l)
+	)
+	return dp[l][d]
 
 
 def main():
 	from sys import stdin, stdout, setrecursionlimit
-	setrecursionlimit(10000000)
+	setrecursionlimit(int(1e6))
 	rl = stdin.readline
-	wl = stdout.write
-	int1 = int
-	global dp
 
-	n = int1(rl().strip())
-	a = list(map(int1, rl().strip().split()))
-	m1 = solve(a, n)
-	dp = [0] + [-1] * 10000000
-	m2 = solve(a[::-1], n)
-	wl(str(min(m1, m2)))
+	n = int(rl())
+	a = [int(x) for x in rl().split()]
+
+	stdout.write(str(min(
+		solve(a, n - 1, 1),
+		solve(a, n - 2, 0)
+	)))
 
 
 main()
