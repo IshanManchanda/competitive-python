f83da82a2b22b9931376510b3bab4408012e8649
Ishan Manchanda <ishanmanchanda70@gmail.com>
2019-11-18 16:32:47 +0530

    Added solution for Wormholes - ZCO 2012

---
+++ CodeChef/COMPETE/ZCO Practice Contest - ZCOPRAC/Wormholes - ZCO12002.py
@@ -0,0 +1,32 @@
+# https://www.codechef.com/ZCOPRAC/problems/ZCO12002
+from bisect import bisect_left
+
+
+def main():
+	from sys import stdin, stdout
+	rl = stdin.readline
+
+	n, nv, nw = (int(x) for x in rl().split())
+	c = [[int(x) for x in rl().split()] for _ in range(n)]
+	vs = [int(x) for x in rl().split()]
+	ws = [int(x) for x in rl().split()]
+
+	c.sort()
+	vs.sort()
+	ws.sort()
+	s = [x[0] for x in c]
+	t = 1e7
+
+	for v in vs:
+		i = bisect_left(s, v)
+		for j in range(i, n):
+			if c[j][0] - v >= t:
+				break
+			k = bisect_left(ws, c[j][1])
+			if k != nw:
+				t = min(t, ws[k] - v)
+
+	stdout.write(str(t + 1))
+
+
+main()
