5ff49cd1f29189a42cd2a6e6d01fcb7388e7f5c5
Ishan Manchanda <ishanmanchanda70@gmail.com>
2019-11-26 14:45:11 +0530

    Added links for some more codechef compete problems

--- CodeChef/COMPETE/March Challenge 2018 DIV 2 - MARCH18B/A Big Sale - BIGSALE.py
+++ CodeChef/COMPETE/March Challenge 2018 DIV 2 - MARCH18B/A Big Sale - BIGSALE.py
@@ -1,3 +1,4 @@
+# https://www.codechef.com/MARCH18B/problems/BIGSALE
 def main():
 	from sys import stdin, stdout
 	rl = stdin.readline
--- CodeChef/COMPETE/PLINTH-IUPC 2019 - PILT2019/Sherlocked String - SHLOCK.py
+++ CodeChef/COMPETE/PLINTH-IUPC 2019 - PILT2019/Sherlocked String - SHLOCK.py
@@ -1,3 +1,4 @@
+# https://www.codechef.com/PILT2019/problems/SHLOCK
 def main():
 	from sys import stdin, stdout
 	rl = stdin.readline
